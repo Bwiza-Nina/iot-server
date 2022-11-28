bda4b7c59fd106435b08b33a8fd81a1ca78ba3a0
Bwiza-Nina <ninamyles2020@gmail.com>
2022-11-28 20:49:53 +0200

    Added the connection, controllers, and the services

---
+++ Connections/DatabaseConnection.php
@@ -0,0 +1,17 @@
+<?php
+class DatabaseConnection{
+    private $host = 'localhost';
+    private $user = 'benax_iot_root';
+    private $password = 'Td(FAdeZ9xp3';
+    private $database = 'benax_iot';
+
+    pbulic function getConnection(){
+        $connection = new mysqli($this->host, $this->user, $this->password, $this->database);
+        if($connection->connect_error){
+            die("Error failed to connect to MySQL: ".$connection->connect_error);
+        }else{
+            return $connection;
+        }
+    }
+
+}
---
+++ Controllers/ReadData.php
@@ -0,0 +1,8 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+include_once '../config/Database.php';
+include_once '../class/Items.php';
+
+$database = new DatabaseConnection
